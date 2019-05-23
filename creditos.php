2872bb3cd726538f5b4bdfa275f553a530010071
CesarDark <dark15crm@hotmail.com>
2019-05-23 10:55:24 -0500

    ADD: Créditos

---
+++ creditos.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Créditos</title>
+    <link rel="stylesheet" type="text/css" href="css/menu.css" media="screen" />
+  </head>
+  <body>
+    <h4>&copy; #CesarRebollar 2019</h4>
+    <a href="index.php"> Regresar</a>
+  </body>
+</html>
