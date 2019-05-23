0ae091bbf6c120765dc88a16f7dbeb1641e3eb89
CesarDark <dark15crm@hotmail.com>
2019-05-23 10:58:06 -0500

    ADD: Examen2

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Examen #2 Libros</title>
+    <link rel="stylesheet" type="text/css" href="css/menu.css" media="screen" />
+    <script src="js/jquery-3.3.1.js"></script>
+    <script src="js/texto.js"></script>
+  </head>
+  <body>
+    <div class="principal">
+      <div class="div1">
+        <h4>Stephen King</h4>
+        <h5>Stephen Edwin King (Portland, Maine, 21 de septiembre de 1947) es un escritor estadounidense de novelas de terror, ficción sobrenatural, misterio, ciencia ficción y literatura fantástica. Sus libros han vendido más de 350 millones de copias1​ y en su mayoría han sido adaptados al cine y a la televisión. Ha publicado 62 novelas, siete de ellas bajo el seudónimo Richard Bachman, y siete libros de no ficción. Ha escrito, además, alrededor de doscientos relatos, la mayoría de los cuales han sido recogidos en diez colecciones.</h5>
+      </div>
+      <div class="div2">
+        <h4>Libro: IT</h4>
+        <h5>It es una novela de terror publicada en 1986 por el escritor estadounidense Stephen King. Cuenta la historia de un grupo de siete niños que son aterrorizados por un malvado monstruo -al que llaman «Eso»- que es capaz de cambiar de forma, alimentándose del terror que produce en sus víctimas.</h5>
+
+      </div>
+      <!-- Cargar Texto -->
+      <div id="div1" class="div3"></div>
+      <button id="boton" name="button">Ver Texto Demo</button>
+
+      <div>
+        <!-- Cargar Precios -->
+        <div id="div2" class="div4"></div>
+        <button id="boton2" name="button">Ver Precios</button>
+      </div>
+    </div>
+    <br>
+    <a href="creditos.php"> Créditos</a>
+  </body>
+  <footer>
+    <p>&copy; #CesarRebollar 2019</p>
+  </footer>
+</html>
---
+++ js/texto.js
@@ -0,0 +1,13 @@
+// Ver Texto Libro
+$(document).ready(function(){
+ $("#boton").click(function(){
+   $("#div1").load("parrafos.html");
+ });
+});
+
+// Ver Precios
+$(document).ready(function(){
+ $("#boton2").click(function(){
+   $("#div2").load("precios.html");
+ });
+});
